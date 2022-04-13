9fd1bb3aa5c1e7ba47b17f601d7d6d3f5f967ebb
nicolasconde <nicolasconde196@gmail.com>
2022-04-13 17:53:53 -0300

    Novas telas de cadastro de casa

--- app/src/main/java/com/example/queerfy/view/ToPlanRegisterActivity.kt
+++ app/src/main/java/com/example/queerfy/view/ToPlanRegisterActivity.kt
@@ -8,6 +8,7 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.queerfy.databinding.ActivityToplanRegisterBinding
 import com.example.queerfy.model.ToPlanRegisterModel
+import com.example.queerfy.model.UrgencyRegisterModel
 import com.example.queerfy.utils.*
 import com.example.queerfy.viewModel.ToPlanRegisterViewModel
 import java.lang.ref.WeakReference
@@ -21,53 +22,51 @@ class ToPlanRegisterActivity : AppCompatActivity() {
         this.binding = ActivityToplanRegisterBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
 
-        setupSpinner()
         applyMasks()
-        setupListeners()
+//        setupSpinner()
+//        setupListeners()
     }
 
-    private fun setupSpinner() {
-        setupSexualOrientationSpinner()
-        setupGenderIdentitySpinner()
-    }
+//    private fun setupSpinner() {
+//        setupSexualOrientationSpinner()
+//        setupGenderIdentitySpinner()
+//    }
 
-    private fun setupSexualOrientationSpinner() {
-        val options = SexOrientationEnum.toList()
-        this.binding.spnSexOrientation.adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            options
-        )
-
-        //this.binding.spnSexOrientation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-        //override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-            //override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                //ToPlanRegisterViewModel.sexOrientationEnum =
-                //SexOrientationEnum.fromId(position) ?: SexOrientationEnum.SELECT
-                //}
-
-            //}
-    }
-
-    private fun setupGenderIdentitySpinner() {
-        val options = GenderIdentityEnum.toList()
-        this.binding.spnGenderIdentity.adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            options
-        )
-
-        //this.binding.spnGenderIdentity.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            //override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-            //override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                //ToPlanRegisterViewModel.genderIdentityEnum =
-                //GenderIdentityEnum.fromId(position) ?: GenderIdentityEnum.SELECT
-                //}
-
-            //}
-    }
+//    private fun setupSexualOrientationSpinner() {
+//        val options = SexOrientationEnum.toList()
+//        this.binding.spnSexOrientation.adapter = ArrayAdapter(
+//            this,
+//            android.R.layout.simple_list_item_1,
+//            options
+//        )
+//
+//        this.binding.spnSexOrientation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+//            override fun onNothingSelected(parent: AdapterView<*>?) {}
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                ToPlanRegisterViewModel.sexOrientationEnum =
+//                    SexOrientationEnum.fromId(position) ?: SexOrientationEnum.SELECT
+//            }
+//        }
+//    }
+//
+//    private fun setupGenderIdentitySpinner() {
+//        val options = GenderIdentityEnum.toList()
+//        this.binding.spnGenderIdentity.adapter = ArrayAdapter(
+//            this,
+//            android.R.layout.simple_list_item_1,
+//            options
+//        )
+//
+//        this.binding.spnGenderIdentity.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+//            override fun onNothingSelected(parent: AdapterView<*>?) {}
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                ToPlanRegisterViewModel.genderIdentityEnum =
+//                GenderIdentityEnum.fromId(position) ?: GenderIdentityEnum.SELECT
+//            }
+//        }
+//    }
 
     private fun applyMasks() {
         this.binding.edtRg.addTextChangedListener(RgMask.insert(this.binding.edtRg))
@@ -78,44 +77,40 @@ class ToPlanRegisterActivity : AppCompatActivity() {
         this.binding.edtPhone.addTextChangedListener(addLineNumberFormatter);
     }
 
-    private fun setupListeners() {
-        this.binding.btnFinish.setOnClickListener {
-            // TODO Validar campos
-            //if (validateFields()) {
-            //seturgencyRegisterModel()
-            //urgencyRegisterViewModel.putIntoBd()
-            //} else {
-                // TODO Mostrar que tem algo errado e dar foco
-            //}
-        }
-    }
+//    private fun setupListeners() {
+//        this.binding.btnFinish.setOnClickListener {
+//            // TODO Validar campos
+//            if (validateFields()) {
+//                setToPlanRegisterModel()
+//                ToPlanRegisterViewModel.putIntoBd()
+//            } else {
+//                // TODO Mostrar que tem algo errado e dar foco
+//            }
+//        }
+//    }
 
     private fun validateFields(): Boolean {
         // TODO Validar todos os campos (Botão Finalizar)
         return true
     }
 
-    // private fun seturgencyRegisterModel() {
-    // toPlanRegisterViewModel.toPlanRegisterModel =
-    // toPlanRegisterViewModel(
-    // name = this.binding.edtName.text.toString(),
-    // sinceDate = this.binding.edtSinceDate.text.toString()
-    // .replace("/","")
-    // .replace(" ","")
-    // .trim(),
-    // sexOrientation =  toPlanRegisterViewModel.sexOrientationEnum,
-    // genderIdentity =  toPlanRegisterViewModel.genderIdentityEnum,
-    // cpf = this.binding.edtCpf.text.toString()
-    // .replace(".","")
-    // .replace("-","")
-    // .trim(),
-    // email = this.binding.edtEmail.text.toString(),
-    // phone = this.binding.edtPhone.text.toString()
-    // .replace("(","")
-    // .replace(")","")
-    // .replace("-","")
-    // .replace(" ","")
-    // .trim()
-    // )
-    // }
+//    private fun setToPlanRegisterModel() {
+//        ToPlanRegisterViewModel.ToPlanRegisterModel =
+//            ToPlanRegisterModel(
+//                name = this.binding.edtName.text.toString(),
+//                sexOrientation =  ToPlanRegisterViewModel.sexOrientationEnum,
+//                genderIdentity =  ToPlanRegisterViewModel.sexOrientationEnum,
+//                cpf = this.binding.edtCpf.text.toString()
+//                    .replace(".","")
+//                    .replace("-","")
+//                    .trim(),
+//                email = this.binding.edtEmail.text.toString(),
+//                phone = this.binding.edtPhone.text.toString()
+//                    .replace("(","")
+//                    .replace(")","")
+//                    .replace("-","")
+//                    .replace(" ","")
+//                    .trim()
+//            )
+//    }
 }
