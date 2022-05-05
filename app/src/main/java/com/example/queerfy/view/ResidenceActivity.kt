35a0a5330e5ad1aae9e910b928ce99d537c9f64c
oLemos <lemmos@outlook.com.br>
2022-05-04 21:53:19 -0300

    make footer component

--- app/src/main/java/com/example/queerfy/view/ResidenceActivity.kt
+++ app/src/main/java/com/example/queerfy/view/ResidenceActivity.kt
@@ -8,5 +8,10 @@ class ResidenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_residence)
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.add(R.id.fragment_residence_footer, FragmentFooter::class.java, null)
+        transaction.commit()
     }
 }
