a202c38f13c79d0115315a9146983878cf4df36c
oLemos <lemmos@outlook.com.br>
2022-05-11 22:14:08 -0300

    init residence list activity

---
+++ app/src/main/java/com/example/queerfy/view/ResidenceListActivity.kt
@@ -0,0 +1,17 @@
+package com.example.queerfy.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.queerfy.R
+
+class ResidenceListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_residence_list)
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.add(R.id.residence_list_trend_one, TrendResidenceFragment::class.java, null)
+        transaction.commit()
+    }
+}
