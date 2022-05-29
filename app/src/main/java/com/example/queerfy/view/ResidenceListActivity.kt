b5d9270554800c111a5a78a9a02cf3334987b8ff
Lemos <lemmos@outlook.com.br>
2022-05-29 13:57:37 -0300

    fix: residence list filter color

--- app/src/main/java/com/example/queerfy/view/ResidenceListActivity.kt
+++ app/src/main/java/com/example/queerfy/view/ResidenceListActivity.kt
@@ -1,8 +1,9 @@
 package com.example.queerfy.view
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Property
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -11,7 +12,6 @@ import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.queerfy.R
-import com.example.queerfy.model.Favorite
 import com.example.queerfy.services.Api
 import retrofit2.Call
 import retrofit2.Callback
@@ -22,6 +22,16 @@ class ResidenceListActivity : AppCompatActivity() {
     private lateinit var recyclerViewTrendOne: RecyclerView
     private lateinit var recyclerViewResidence: RecyclerView
 
+    private lateinit var wifiButton: Button
+    private lateinit var kitchenButton: Button
+    private lateinit var suiteButton: Button
+    private lateinit var garageButton: Button
+    private lateinit var animalButton: Button
+
+    private val colorFont: String = "#1A1A1A"
+    private val colorBlue: String = "#439EFA"
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_residence_list)
@@ -30,7 +40,7 @@ class ResidenceListActivity : AppCompatActivity() {
         val cityNotFormated = intent.getStringExtra("cityNotFormated")
 
         findViewById<TextView>(R.id.residence_list_trend_title).text =
-            "Locações com as melhores avaliações em ${cityNotFormated}"
+            "Locações com as melhores avaliações em $cityNotFormated"
 
         recyclerViewTrendOne = findViewById(R.id.trend_residences_list)
         recyclerViewTrendOne.layoutManager = LinearLayoutManager(baseContext)
@@ -42,11 +52,15 @@ class ResidenceListActivity : AppCompatActivity() {
         recyclerViewResidence.itemAnimator = DefaultItemAnimator()
         recyclerViewResidence.setHasFixedSize(true)
 
+        wifiButton = findViewById(R.id.btn_have_wifi)
+        kitchenButton = findViewById(R.id.btn_have_kitchen)
+        suiteButton = findViewById(R.id.btn_have_suite)
+        garageButton = findViewById(R.id.btn_have_garage)
+        animalButton = findViewById(R.id.btn_have_animal)
+
         val getProperties = Api.create().getResidencesSearch(city)
-
-        var residencesTrendList = mutableListOf<com.example.queerfy.model.Property>()
-
-        var residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesTrendList = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
 
         getProperties.enqueue(object : Callback<List<com.example.queerfy.model.Property>> {
             override fun onResponse(
@@ -54,13 +68,10 @@ class ResidenceListActivity : AppCompatActivity() {
                 response: Response<List<com.example.queerfy.model.Property>>
             ) {
                 if (response.isSuccessful) {
-
                     val residenciesList = response.body()
 
-                    if (!residenciesList!!.isEmpty()) {
-
+                    if (residenciesList!!.isNotEmpty()) {
                         residenciesList.forEach { property ->
-
                             if (property.likes!! > 1000) {
                                 findViewById<TextView>(R.id.residence_list_trend_title).visibility =
                                     View.VISIBLE
@@ -72,14 +83,10 @@ class ResidenceListActivity : AppCompatActivity() {
                             }
 
                             residencesListSearch.add(property)
-
-
                         }
 
                         recyclerViewTrendOne.adapter = TrendResidenceFragment(residencesTrendList)
-
                         recyclerViewResidence.adapter = ResidenceFragment(residencesListSearch)
-
                     }
                 }
             }
@@ -94,18 +101,38 @@ class ResidenceListActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         })
 
     }
 
+    private fun setButtonHighlight(button: Button) {
+        wifiButton.setTextColor(
+            Color.parseColor(colorFont)
+        )
+        kitchenButton.setTextColor(
+            Color.parseColor(colorFont)
+        )
+        suiteButton.setTextColor(
+            Color.parseColor(colorFont)
+        )
+        garageButton.setTextColor(
+            Color.parseColor(colorFont)
+        )
+        animalButton.setTextColor(
+            Color.parseColor(colorFont)
+        )
+
+        button.setTextColor(
+            Color.parseColor(colorBlue)
+        )
+    }
+
     fun getHaveWifiResidences(v: View) {
+        setButtonHighlight(wifiButton)
 
         val city = intent.getStringExtra("city") as String
-
         val getProperties = Api.create().getResidencesSearch(city)
-
-        var residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
 
         getProperties.enqueue(object : Callback<List<com.example.queerfy.model.Property>> {
             override fun onResponse(
@@ -113,21 +140,16 @@ class ResidenceListActivity : AppCompatActivity() {
                 response: Response<List<com.example.queerfy.model.Property>>
             ) {
                 if (response.isSuccessful) {
-
                     val residenciesList = response.body()
 
-                    if (!residenciesList!!.isEmpty()) {
-
+                    if (residenciesList!!.isNotEmpty()) {
                         residenciesList.forEach { property ->
-
                             if (property.haveWifi == true) {
                                 residencesListSearch.add(property)
                             }
-
                         }
 
                         recyclerViewResidence.adapter = ResidenceFragment(residencesListSearch)
-
                     }
                 }
             }
@@ -142,17 +164,15 @@ class ResidenceListActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         })
     }
 
     fun getHaveKitchenResidences(v: View) {
+        setButtonHighlight(kitchenButton)
 
         val city = intent.getStringExtra("city") as String
-
         val getProperties = Api.create().getResidencesSearch(city)
-
-        var residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
 
         getProperties.enqueue(object : Callback<List<com.example.queerfy.model.Property>> {
             override fun onResponse(
@@ -160,21 +180,16 @@ class ResidenceListActivity : AppCompatActivity() {
                 response: Response<List<com.example.queerfy.model.Property>>
             ) {
                 if (response.isSuccessful) {
-
                     val residenciesList = response.body()
 
-                    if (!residenciesList!!.isEmpty()) {
-
+                    if (residenciesList!!.isNotEmpty()) {
                         residenciesList.forEach { property ->
-
                             if (property.haveKitchen == true) {
                                 residencesListSearch.add(property)
                             }
-
                         }
 
                         recyclerViewResidence.adapter = ResidenceFragment(residencesListSearch)
-
                     }
                 }
             }
@@ -189,17 +204,15 @@ class ResidenceListActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         })
     }
 
     fun getHaveSuiteResidences(v: View) {
+        setButtonHighlight(suiteButton)
 
         val city = intent.getStringExtra("city") as String
-
         val getProperties = Api.create().getResidencesSearch(city)
-
-        var residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
 
         getProperties.enqueue(object : Callback<List<com.example.queerfy.model.Property>> {
             override fun onResponse(
@@ -207,21 +220,16 @@ class ResidenceListActivity : AppCompatActivity() {
                 response: Response<List<com.example.queerfy.model.Property>>
             ) {
                 if (response.isSuccessful) {
-
                     val residenciesList = response.body()
 
-                    if (!residenciesList!!.isEmpty()) {
-
+                    if (residenciesList!!.isNotEmpty()) {
                         residenciesList.forEach { property ->
-
                             if (property.haveSuite == true) {
                                 residencesListSearch.add(property)
                             }
-
                         }
 
                         recyclerViewResidence.adapter = ResidenceFragment(residencesListSearch)
-
                     }
                 }
             }
@@ -236,17 +244,15 @@ class ResidenceListActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         })
     }
 
     fun getHaveGarageResidences(v: View) {
+        setButtonHighlight(garageButton)
 
         val city = intent.getStringExtra("city") as String
-
         val getProperties = Api.create().getResidencesSearch(city)
-
-        var residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
 
         getProperties.enqueue(object : Callback<List<com.example.queerfy.model.Property>> {
             override fun onResponse(
@@ -254,21 +260,16 @@ class ResidenceListActivity : AppCompatActivity() {
                 response: Response<List<com.example.queerfy.model.Property>>
             ) {
                 if (response.isSuccessful) {
-
                     val residenciesList = response.body()
 
-                    if (!residenciesList!!.isEmpty()) {
-
+                    if (residenciesList!!.isNotEmpty()) {
                         residenciesList.forEach { property ->
-
                             if (property.haveGarage == true) {
                                 residencesListSearch.add(property)
                             }
-
                         }
 
                         recyclerViewResidence.adapter = ResidenceFragment(residencesListSearch)
-
                     }
                 }
             }
@@ -283,17 +284,15 @@ class ResidenceListActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         })
     }
 
     fun getHaveAnimalsResidences(v: View) {
+        setButtonHighlight(animalButton)
 
         val city = intent.getStringExtra("city") as String
-
         val getProperties = Api.create().getResidencesSearch(city)
-
-        var residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
+        val residencesListSearch = mutableListOf<com.example.queerfy.model.Property>()
 
         getProperties.enqueue(object : Callback<List<com.example.queerfy.model.Property>> {
             override fun onResponse(
@@ -301,21 +300,16 @@ class ResidenceListActivity : AppCompatActivity() {
                 response: Response<List<com.example.queerfy.model.Property>>
             ) {
                 if (response.isSuccessful) {
-
                     val residenciesList = response.body()
 
-                    if (!residenciesList!!.isEmpty()) {
-
+                    if (residenciesList!!.isNotEmpty()) {
                         residenciesList.forEach { property ->
-
                             if (property.haveAnimals == true) {
                                 residencesListSearch.add(property)
                             }
-
                         }
 
                         recyclerViewResidence.adapter = ResidenceFragment(residencesListSearch)
-
                     }
                 }
             }
@@ -330,8 +324,6 @@ class ResidenceListActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         })
     }
-
 }
