ba93c0187e492d81d8be5473407886ec87fdcbf1
IgorSilvaZZ <igor06silva12@gmail.com>
2022-06-01 15:15:48 -0300

    Ajust register choose

--- app/src/main/java/com/example/queerfy/view/LoginFormActivity.kt
+++ app/src/main/java/com/example/queerfy/view/LoginFormActivity.kt
@@ -27,7 +27,7 @@ class LoginFormActivity : AppCompatActivity() {
     }
 
     fun avancedRegisterChoose(v: View) {
-        val registerChoose = Intent(this, ResidenceListActivity::class.java)
+        val registerChoose = Intent(this, RegisterChooseActivity::class.java)
 
         startActivity(registerChoose)
     }
